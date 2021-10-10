cb23c8c54a6fc156ac46d7bc735b48bfdf2fde83
arkanael <arkanael@gmail.com>
2021-10-09 22:32:43 -0300

    Adicionando o ejs como view engine

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,26 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
+
+const basePath = `${__dirname}/views`;
 
 //rota para abrir o index.html
 routes.get("/", (request, response) => {
-    return response.sendFile(`${__dirname}/views/index.html`);
+  return response.sendFile(`${basePath}/index.html`);
+});
+
+//rota para abrir o job.html
+routes.get("/job", (request, response) => {
+  return response.sendFile(`${basePath}/job.html`);
+});
+
+//rota para abrir o job-edit.html
+routes.get("/job/edit", (request, response) => {
+  return response.sendFile(`{basePath}/job-edit.html`);
+});
+
+//rota para abrir o profile.html
+routes.get("/profile", (request, response) => {
+  return response.sendFile(`${basePath}/profile.html`);
 });
 
 module.exports = routes;
